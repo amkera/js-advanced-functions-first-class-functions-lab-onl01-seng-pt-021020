a2a5e783c938b494dcb5e6eaabf8a1ea578525a8
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-01 21:14:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3); 
 
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
+
