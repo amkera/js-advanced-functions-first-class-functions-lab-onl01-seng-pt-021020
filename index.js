4a51a72bbc4fae787765c7118a51d06172b7e2e2
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-01 21:05:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(arrayOfDrivers) {
-  let newArray = []
-  newArray.push(arrayOfDrivers[0]);
-  newArray.push(arrayOfDrivers[1]);
-  return newArray;
-}
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
