e845af416538d8b25cc5568f1e7b3e516d955d94
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-01 21:10:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
 };
 
-let selectingDrivers = [returnLastTwoDrivers(), returnLastTwoDrivers()]; 
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
 
